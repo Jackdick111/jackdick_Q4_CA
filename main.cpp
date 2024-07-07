de78c1fa4e7ea54746d1ff8cd4a2d99fff4d47fa
Jackdick111 <jackdick111@gmail.com>
2024-07-07 17:03:48 +0000

    CA Q4 Jack Dick

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include "mbed.h"
+
+int main() {
+    // Initialize variables
+    int a = 10;
+    int b = 20;
+    int c = 30;
+
+    // Add values of a and b, store in d, and print d
+    int d = a + b;
+    printf("Value of d (a + b): %d\n", d);
+
+    // Subtract c from d, store in e, and print e
+    int e = d - c;
+    printf("Value of e (d - c): %d\n", e);
+    }
+
+
